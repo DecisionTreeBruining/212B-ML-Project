1f97fd5f3244b0058fc7458b9f5d91a92e9af4c4
William "Taylor" Martinez <wtmartinez@ucla.edu>
2024-06-08 23:28:03 -0700

    added more to taylor app

--- heartd_shiny/shiny/taylor_app.py
+++ heartd_shiny/shiny/taylor_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from shinyswatch import theme
 from app_tools import pickle_to_dict
 import pickle
 
@@ -65,13 +66,20 @@ tdap_list = [
 ]
 
 race_list = [
-    "White", "Black", "Asian", 
-    "American Indian/Alaskan Native", 
-    "Hispanic", "Other"
+    'White only, Non-Hispanic', 'Black only, Non-Hispanic',
+    'Other race only, Non-Hispanic', 'Multiracial, Non-Hispanic',
+    'Hispanic']
+
+ECigarette_list = [
+    'Never used e-cigarettes in my entire life',
+    'Not at all (right now)',
+    'Use them some days',
+    'Use them every day'
 ]
 
 # UI section starts from here 
 app_ui = ui.page_fluid(
+    theme.flatly(),  # Applying the flatly theme for a blue look
     ui.markdown(
         """
         ## Heart Disease Prediction Model
@@ -86,6 +94,7 @@ app_ui = ui.page_fluid(
             ui.input_numeric("PhysicalHealthDays", "Physical Health Days", value=0),
             ui.input_numeric("MentalHealthDays", "Mental Health Days", value=0),
             ui.input_select("LastCheckupTime", "When was your checkup?", lastcheckup_list),
+            ui.input_select("ECigaretteUsage", "What's your E-Cigarette Usage?", ECigarette_list),
             ui.input_select("PhysicalActivities", "Physical Activities", yes_no_list),
             ui.input_numeric("SleepHours", "Sleep Hours", value=0),
             ui.input_select("RemovedTeeth", "Have you had any teeth removed?", yes_no_list),
@@ -134,76 +143,44 @@ def server(input, output, session):
     @reactive.Effect
     @reactive.event(input.submit)
     def submit_info():
-        # Convert input values to integers
-        state = int(input.State())
-        sex = int(input.Sex())
-        age_category = int(input.AgeCategory())
-        last_checkup_time = int(input.LastCheckupTime())
-        physical_activities = int(input.PhysicalActivities())
-        removed_teeth = int(input.RemovedTeeth())
-        general_health = int(input.GeneralHealth())
-        smoker_status = int(input.SmokerStatus())
-        hiv_testing = int(input.HIVTesting())
-        flu_vax_last12 = int(input.FluVaxLast12())
-        pneumo_vax_ever = int(input.PneumoVaxEver())
-        tetanus_last10_tdap = int(input.TetanusLast10Tdap())
-        high_risk_last_year = int(input.HighRiskLastYear())
-        covid_pos = int(input.CovidPos())
-        had_asthma = int(input.HadAsthma())
-        had_skin_cancer = int(input.HadSkinCancer())
-        had_copd = int(input.HadCOPD())
-        had_depressive_disorder = int(input.HadDepressiveDisorder())
-        had_kidney_disease = int(input.HadKidneyDisease())
-        had_arthritis = int(input.HadArthritis())
-        had_diabetes = int(input.HadDiabetes())
-        deaf_or_hard_of_hearing = int(input.DeafOrHardOfHearing())
-        blind_or_vision_difficulty = int(input.BlindOrVisionDifficulty())
-        difficulty_concentrating = int(input.DifficultyConcentrating())
-        difficulty_walking = int(input.DifficultyWalking())
-        difficulty_dressing_bathing = int(input.DifficultyDressingBathing())
-        difficulty_errands = int(input.DifficultyErrands())
-        chest_scan = int(input.ChestScan())
-        race_ethnicity_category = int(input.RaceEthnicityCategory())
-        alcohol_drinkers = int(input.AlcoholDrinkers())
-
         # Collect input data
         input_data = pd.DataFrame([{
-            'State': state_mapping[state],
-            'Sex': sex_mapping[sex],
-            'AgeCategory': age_category_mapping[age_category],
+            'State': input.State(),
+            'Sex': input.Sex(),
+            'AgeCategory': input.AgeCategory(),
             'PhysicalHealthDays': input.PhysicalHealthDays(),
+            'LastCheckupTime': input.LastCheckupTime(),
+            'ECigaretteUsage': input.ECigaretteUsage(),
+            'PhysicalActivities': input.PhysicalActivities(),
             'MentalHealthDays': input.MentalHealthDays(),
-            'LastCheckupTime': last_checkup_time_mapping[last_checkup_time],
-            'PhysicalActivities': physical_activities_mapping[physical_activities],
             'SleepHours': input.SleepHours(),
-            'RemovedTeeth': removed_teeth_mapping[removed_teeth],
-            'GeneralHealth': general_health_mapping[general_health],
+            'RemovedTeeth': input.RemovedTeeth(),
+            'GeneralHealth': input.GeneralHealth(),
             'HeightInMeters': input.HeightInMeters(),
             'WeightInKilograms': input.WeightInKilograms(),
-            'SmokerStatus': smoker_status_mapping[smoker_status],
-            'HIVTesting': yes_no_mapping[hiv_testing],
-            'FluVaxLast12': yes_no_mapping[flu_vax_last12],
-            'PneumoVaxEver': yes_no_mapping[pneumo_vax_ever],
-            'TetanusLast10Tdap': tetanus_last10_tdap,
-            'HighRiskLastYear': yes_no_mapping[high_risk_last_year],
-            'CovidPos': yes_no_mapping[covid_pos],
-            'HadAsthma': yes_no_mapping[had_asthma],
-            'HadSkinCancer': yes_no_mapping[had_skin_cancer],
-            'HadCOPD': yes_no_mapping[had_copd],
-            'HadDepressiveDisorder': yes_no_mapping[had_depressive_disorder],
-            'HadKidneyDisease': yes_no_mapping[had_kidney_disease],
-            'HadArthritis': yes_no_mapping[had_arthritis],
-            'HadDiabetes': yes_no_mapping[had_diabetes],
-            'DeafOrHardOfHearing': yes_no_mapping[deaf_or_hard_of_hearing],
-            'BlindOrVisionDifficulty': yes_no_mapping[blind_or_vision_difficulty],
-            'DifficultyConcentrating': yes_no_mapping[difficulty_concentrating],
-            'DifficultyWalking': yes_no_mapping[difficulty_walking],
-            'DifficultyDressingBathing': yes_no_mapping[difficulty_dressing_bathing],
-            'DifficultyErrands': yes_no_mapping[difficulty_errands],
-            'ChestScan': yes_no_mapping[chest_scan],
-            'RaceEthnicityCategory': race_ethnicity_mapping[race_ethnicity_category],
-            'AlcoholDrinkers': yes_no_mapping[alcohol_drinkers],
-            'BMI': input.WeightInKilograms() / (input.HeightInMeters() ** 2)
+            'SmokerStatus': input.SmokerStatus(),
+            'HIVTesting': input.HIVTesting(),
+            'FluVaxLast12': input.FluVaxLast12(),
+            'PneumoVaxEver': input.PneumoVaxEver(),
+            'TetanusLast10Tdap': input.TetanusLast10Tdap(),
+            'HighRiskLastYear': input.HighRiskLastYear(),
+            'CovidPos': input.CovidPos(),
+            'HadAsthma': input.HadAsthma(),
+            'HadSkinCancer': input.HadSkinCancer(),
+            'HadCOPD': input.HadCOPD(),
+            'HadDepressiveDisorder': input.HadDepressiveDisorder(),
+            'HadKidneyDisease': input.HadKidneyDisease(),
+            'HadArthritis': input.HadArthritis(),
+            'HadDiabetes': input.HadDiabetes(),
+            'DeafOrHardOfHearing': input.DeafOrHardOfHearing(),
+            'BlindOrVisionDifficulty': input.BlindOrVisionDifficulty(),
+            'DifficultyConcentrating': input.DifficultyConcentrating(),
+            'DifficultyWalking': input.DifficultyWalking(),
+            'DifficultyDressingBathing': input.DifficultyDressingBathing(),
+            'DifficultyErrands': input.DifficultyErrands(),
+            'ChestScan': input.ChestScan(),
+            'RaceEthnicityCategory': input.RaceEthnicityCategory(),
+            'AlcoholDrinkers': input.AlcoholDrinkers()
         }])
         # Make prediction
         prediction = pipeline.predict_proba(input_data)[0][1] * 100
